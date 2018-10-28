3f34d6ed2da995740c2e425b1bdef848b413f111
bob <bob@ubuntu>
2018-10-28 22:28:19 +0800

    types

---
+++ @types/account/signin.vue
@@ -0,0 +1,113 @@
+<style lang="sass" src="./style.sass">
+</style>
+
+<template lang="pug">
+  .fancy-account
+    form(method="post" @submit="_submit" onsubmit="return false")
+      dl
+        dt(v-if="cfg.account.label") {{cfg.account.label}}
+        dd.fc-account
+          input(ref="account" type="text" v-model="account" v-bind="{name: cfg.account.name, maxlength: cfg.account.maxlength, placeholder: cfg.account.placeholder}")
+
+        dt(v-if="cfg.password.label") {{cfg.password.label}}
+        dd.fc-password
+          input(ref="password" type="password" v-model="password" v-bind="{name: cfg.password.name, maxlength: cfg.password.maxlength, placeholder: cfg.password.placeholder}")
+
+        template(v-if="cfg.captcha.name")
+          dt(v-if="cfg.captcha.label") {{cfg.captcha.label}}
+          dd.fc-captcha
+            div
+              input(ref="captcha" type="text" v-model="captcha" v-bind="{maxlength: cfg.captcha.maxlength, placeholder: cfg.captcha.placeholder}")
+            div
+              img(:src="codeimg" @click="_captcha" )
+
+        dd.fc-tip(v-if="message")
+          span(v-text="message")
+        dd.fc-btns
+          input.fc-submit(:value="cfg.submit.name" type="submit")
+
+</template>
+
+<script>
+
+  const Options = {
+    account: {
+      label: '',
+      name: 'account',
+      maxlength: 30,
+      placeholder: 'Account',
+    },
+    password: {
+      label: '',
+      name: 'password',
+      maxlength: 30,
+      placeholder: 'Password',
+    },
+    captcha: {
+      label: '',
+      name: 'captcha',
+      url: '?c=Verify',
+      maxlength: 4,
+      placeholder: 'Captcha',
+    },
+    submit: {
+      name: 'Sign In',
+    },
+    onSubmit(data, refs){}
+  }
+  export default {
+    props: ['cfg'],
+    data() {
+      return {
+        tt: 0,
+        message: false,
+        locked: false,
+        account: '',
+        password: '',
+        captcha: '',
+      }
+    },
+    created() {
+      Object.keys(Options).forEach(i => {
+        (i in this.cfg) || this.$set(this.cfg, i, Options[i])
+      })
+    },
+    computed: {
+      codeimg() {
+        let v = this.cfg.captcha
+        return v.url + (v.url.indexOf('?') < 0 ? '?' : '&') + 'tt=' + this.tt
+      }
+    },
+    methods: {
+      _showtip(str) {
+        requestAnimationFrame(() => this.message = str)
+      },
+      _captcha() {
+        this.tt = Date.now()
+        if (this.$refs.captcha) {
+          this.$refs.captcha.value = ''
+        }
+      },
+      _submit: async function() {
+        let data = {}
+        let cfg = this.cfg
+        data[cfg.account.name] = this.account.trim()
+        data[cfg.password.name] = this.password.trim()
+        data[cfg.captcha.name] = this.captcha.trim()
+
+        this.message = ''
+        if (this.locked) return
+        try {
+          this.locked = true
+          await cfg.onSubmit(data, this.$refs)
+        } catch (err) {
+          this.locked = false
+          this._captcha()
+          this._showtip(err)
+        }
+        this.locked = false
+      },
+    },
+  }
+
+</script>
---
+++ @types/alert/index.vue
@@ -0,0 +1,107 @@
+<style lang="sass">
+  @import "~fancy_style"
+  @import "~fancy_mixins"
+  .fancy-alert
+    &.fc-mask
+      @include mixin-mask()
+    &:focus
+      outline: 0
+    > div
+      position: absolute
+      top: 50%
+      left: 50%
+      max-width: 90%
+      max-height: 90%
+      min-width: 16rem
+      display: flex
+      flex-direction: column
+      border-radius: $borderRadius
+      overflow: hidden
+      background-color: #fff
+      transform: translate3d( -50%, -50%, 0)
+      .fc-msg
+        text-align: center
+        padding: $space * 4 0
+      .fc-btn
+        cursor: pointer
+        text-align: center
+        margin: 0
+        line-height: $form-height
+        background: $colorTheme
+        color: #fff
+        &:hover
+          opacity: 0.8
+
+    @media #{$device-phone}
+      > div
+        min-width: 60%
+    @media #{$device-pad}
+      > div
+        min-width: 40%
+
+</style>
+
+<template lang="pug">
+  .fancy-alert(tabindex="1",:class="{'fc-mask':cfg.ismask}")
+    div
+      .fc-msg(v-html="cfg.message")
+      .fc-btn(v-if="cfg.confirm" @click="onClick()") {{cfg.confirm}}
+</template>
+
+<script lang="ts">
+declare var document: any
+import { Component, Vue } from 'vue-property-decorator'
+
+const options: IFancyAlert = {
+  message: '',
+  confirm: 'ok',
+  ismask: true,
+}
+export interface IFancyAlert {
+  message?: string
+  confirm?: string
+  ismask?: boolean
+  onConfirm?: () => {}
+}
+
+@Component({
+  props: ['cfg'],
+})
+export default class App extends Vue {
+  private cfg: any
+  public created() {
+    console.log(123)
+    console.log(this.cfg)
+    Object.keys(options).forEach(i => {
+      this.cfg.hasOwnProperty(i) || this.$set(this.cfg, i, (options as any)[i])
+    })
+    document.addEventListener('keydown', this.onKdown, false)
+  }
+
+  public mounted() {
+    this.$el.focus()
+  }
+
+  public destroyed() {
+    document.removeEventListener('keydown', this.onKdown, false)
+  }
+
+  private onKdown(e: any) {
+    // enter space center esc
+    if ([13, 32, 100, 27].includes(e.keyCode)) {
+      e.preventDefault()
+      e.stopPropagation()
+      return this.onClick()
+    }
+  }
+  private async onClick() {
+    try {
+      this.cfg.onConfirm && (await this.cfg.onConfirm())
+      // this.cfg.__name && (this.$parent[this.cfg.__name] = false)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
